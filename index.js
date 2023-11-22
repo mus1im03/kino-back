060683016264da59e4668403a881873be4a893b9
Muslim Bariev <muslim@MacBook-Air-Muslim.local>
2023-11-22 11:16:20 +0300

    sdella

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require('path')
-console.log(path.resolve(__dirname, 'public', 'img'))
+
 app.use('/public/img', express.static(path.resolve(__dirname, 'public', 'img')))
 
 app.use(cors());
@@ -23,7 +23,7 @@ connectAndStartServer = async () => {
   try {
     await mongoose.connect(MONGO_SERVER);
     app.listen(PORT, () => {
-      console.log(`Сервер запущен: ${PORT}`);
+      console.log(`Сервер запущен: http://localhost:${PORT}`);
     });
   } catch (error) {
     console.log(`Ошибка при подключении: ${error.toString()}`);
