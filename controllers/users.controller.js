e9c0ea70838b3052681eddff1220bc6406a60810
Rahman <galfertaxler@example.com>
2023-11-24 10:54:33 +0300

    Favorite

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,6 @@
-const User = require("../models/User.model")
-const bcrypt = require("bcrypt")
-const jwt = require('jsonwebtoken')
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports.usersController = {
   getUsers: async (req, res) => {
@@ -11,6 +11,32 @@ module.exports.usersController = {
       res.json(error);
     }
   },
+
+  createFavorite: async (req, res) => {
+    const { film, id } = req.body;
+    try {
+      const user = await User.findById(id);
+
+      if (user.favorites.includes(film)) {
+        const favorite = await User.findByIdAndUpdate(
+          id,
+          { $pull: { favorites: film } },
+          { new: true }
+        );
+        return res.json(favorite);
+      }
+      const favorite = await User.findByIdAndUpdate(
+        id,
+        { $push: { favorites: film } },
+        { new: true }
+      );
+      res.status(201).json(favorite);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+
+ 
 
   postUser: async (req, res) => {
     try {
@@ -23,7 +49,6 @@ module.exports.usersController = {
 
       const user = await User.create({ login: login, password: hash });
 
-      
       await res.json(user);
     } catch (error) {
       res.json(error);
@@ -33,29 +58,28 @@ module.exports.usersController = {
   login: async (req, res) => {
     const { login, password } = req.body;
 
-    const candidate = await User.findOne({ login })
+    const candidate = await User.findOne({ login });
 
-    if(!candidate) {
-        return res.status(401).json('Неверный логин')
+    if (!candidate) {
+      return res.status(401).json("Неверный логин");
     }
 
-    // console.log(password, candidate.password);
 
-    const valid = await bcrypt.compare(password, candidate.password)
+    const valid = await bcrypt.compare(password, candidate.password);
 
-    if(!valid) {
-        return res.status(401).json('Неверный пароль')
+    if (!valid) {
+      return res.status(401).json("Неверный пароль");
     }
 
     const payload = {
-        id: candidate._id,
-        login: candidate.login
-    }
+      id: candidate._id,
+      login: candidate.login,
+    };
 
     const token = await jwt.sign(payload, process.env.SECRET_JWT_KEY, {
-        expiresIn: '24h',
-    })
+      expiresIn: "24h",
+    });
 
-    res.json(token)
-  }
+    res.json(token);
+  },
 };
